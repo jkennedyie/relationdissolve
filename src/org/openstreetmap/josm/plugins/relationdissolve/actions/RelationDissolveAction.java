a414aa48bf9d5f26c93c48e1cbf6f4b524b00520
John Kennedy <jkennedyie@gmail.com>
2019-10-07 11:05:30 +0100

    Closing #1 menu separator, whitespace cleanup

--- src/org/openstreetmap/josm/plugins/relationdissolve/actions/RelationDissolveAction.java
+++ src/org/openstreetmap/josm/plugins/relationdissolve/actions/RelationDissolveAction.java
@@ -27,29 +27,29 @@ import org.openstreetmap.josm.tools.ImageProvider;
  */
 public class RelationDissolveAction extends AbstractRelationAction implements IPrimitiveAction {
 
-	/**
-	 * Constructs a new {@code RelationDissolveAction}.
-	 */
-	public RelationDissolveAction() {
-		putValue(SHORT_DESCRIPTION, tr("Dissolve then select the members of all selected relations"));
-		putValue(NAME, tr("Select dissolved members"));
-		new ImageProvider("RelationDissolve").getResource().attachImageIcon(this, true);
-	}
+    /**
+     * Constructs a new {@code RelationDissolveAction}.
+     */
+    public RelationDissolveAction() {
+        putValue(SHORT_DESCRIPTION, tr("Dissolve then select the members of all selected relations"));
+        putValue(NAME, tr("Select dissolved members"));
+        new ImageProvider("RelationDissolve").getResource().attachImageIcon(this, true);
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (!isEnabled() || relations.isEmpty() || !MainApplication.isDisplayingMapView()) return;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (!isEnabled() || relations.isEmpty() || !MainApplication.isDisplayingMapView()) return;
 
-		Set<IPrimitive> members = new LinkedHashSet<>();	// will contain the list of members to be selected
+        Set<IPrimitive> members = new LinkedHashSet<>();    // to contain the list of members to be selected
 
-		for (IRelation<?> r: relations) {					// for each relation selected
-			for(IRelationMember<?> rx: r.getMembers()) {	// for each member in that relation
-				if (!members.remove(rx.getMember())) {      // if member already in members then remove it
-					members.add(rx.getMember());            // otherwise add member to members
-				}
-			}
-		}
+        for (IRelation<?> r: relations) {                   // for each relation selected
+            for(IRelationMember<?> rx: r.getMembers()) {    // for each member in that relation
+                if (!members.remove(rx.getMember())) {      // if member already in members then remove it
+                    members.add(rx.getMember());            // otherwise add member to members
+                }
+            }
+        }
 
-		MainApplication.getLayerManager().getActiveData().setSelected(members);
-	}
+        MainApplication.getLayerManager().getActiveData().setSelected(members);
+    }
 }
